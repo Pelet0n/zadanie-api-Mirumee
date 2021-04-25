5b7fb0d6846feb93104257f79697a457e38cf4ae
Pelet0n <hubert.zdk05@gmail.com>
2021-04-25 11:55:55 +0200

    Usunięto komentarze

--- zadanie.py
+++ zadanie.py
@@ -14,12 +14,6 @@ except:
 response_json = requests.get('https://api.spacexdata.com/v3/launches').json()
 
 for row in response_json:
-   #slownik = {}
-
-   #slownik.update({'flight_number': i['flight_number'],'mission_name': i['mission_name'], 'rocket_id': i['rocket']['rocket_id'],'rocket_name': i['rocket']['rocket_name'],'launch_date_utc':i['launch_date_utc'],'video_link':i['links']['video_link']}) 
-
-   #print(slownik) 
-
    sql = f"INSERT INTO dane VALUES('{row['flight_number']}','{row['mission_name']}','{row['rocket']['rocket_id']}','{row['rocket']['rocket_name']}','{row['launch_date_utc']}','{row['links']['video_link']}')"
 
    cur.execute(sql) 
